c073a6cad0ed9f204fff68f7b354983d607cd5b0
young <stupid80096@gmail.com>
2025-04-29 13:31:43 +0800

    feat: add sidebar ccomponent

---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { HamburgerComponent } from '../hamburger/hamburger.component';
+import { LayoutService } from '../layout.service';
+
+@Component({
+	selector: 'yee-sidebar',
+	standalone: true,
+	imports: [CommonModule, HamburgerComponent],
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+	isOpen = false;
+
+	constructor(private layoutService: LayoutService, private destroyRef: DestroyRef) {}
+
+	ngOnInit(): void {
+		this.layoutService.menuOpen$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(isOpen => {
+			this.isOpen = isOpen;
+		});
+	}
+
+	toggle() {
+		this.layoutService.toggleMenu();
+	}
+}
