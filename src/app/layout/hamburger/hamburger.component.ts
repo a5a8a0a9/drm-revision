9aff3f137b71d2e7f4631195c25a91f8d7a43ef5
young <stupid80096@gmail.com>
2025-04-29 13:30:54 +0800

    feat: add hambuger component

---
+++ src/app/layout/hamburger/hamburger.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LayoutService } from '../layout.service';
+
+@Component({
+	selector: 'yee-hamburger',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './hamburger.component.html',
+	styleUrls: ['./hamburger.component.scss'],
+})
+export class HamburgerComponent implements OnInit {
+	isOpen = false;
+
+	constructor(private layoutService: LayoutService, private destroyRef: DestroyRef) {}
+
+	ngOnInit(): void {
+		this.layoutService.menuOpen$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(isOpen => {
+			this.isOpen = isOpen;
+		});
+	}
+
+	toggle() {
+		this.layoutService.toggleMenu();
+	}
+}
